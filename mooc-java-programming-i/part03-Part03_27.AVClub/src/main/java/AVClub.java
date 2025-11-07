30c9a9b71c38d217b8f2e53ff72a4e319dc3ee07
kritiadhikari135 <kriteeadhikari90@gmail.com>
2025-11-07 14:42:04 +0545

    addition of new code

--- mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -5,6 +5,20 @@ public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while(true){
+            String line = scanner.nextLine();
+            if(line.isEmpty()){
+                break;
+            }
+            String[] parts = line.split(" ");
+            for(String word : parts){
+                if(word.contains("av")){
+                    System.out.println(word);
+                }
+            }
+
+
+        }
 
 
     }
